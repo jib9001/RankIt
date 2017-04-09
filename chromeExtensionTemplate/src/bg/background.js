c3b5fedd346dcba6582dd9d8a5bd0f66baf012b9
Brad <bruceba@mail.uc.edu>
2017-04-09 01:38:12 -0400

    Finalized Badge text update
    Done except for real database values required -brad

--- chromeExtensionTemplate/src/bg/background.js
+++ chromeExtensionTemplate/src/bg/background.js
@@ -4,23 +4,46 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-var i = "brad";
+var points = 8767543;//test number
+points = points.toString();//changing points to string
+var pointString;//initalzing string pointString
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+if(points.length > 9){//if number is really big, default 999m+
+  pointString = "999m+";
+}//end if
+
+else{//if its less than 9 characters find printout
+switch (points.length){//switch statement
+
+  default:
+  pointString = points;
+    break;
+
+  case 5:
+  pointString = points.substring(0, 2) + "." + points.substring(2, 3) + "k";
+    break;
+
+  case 6:
+  pointString = points.substring(0, 3) + "k";
+    break;
+
+  case 7:
+  pointString = points.substring(0, 1) + "." + points.substring(1, 3) + "m";
+    break;
+
+  case 8:
+  pointString = points.substring(0, 2) + "." + points.substring(2, 3) + "m";
+    break;
+
+}//end switch
+}//end else
 
 
 window.onload = function() {
   UpdateBadge();
-}
+}//end window.onload
 
 
 function UpdateBadge(){
-  //chrome.browserAction.setBadgeText({text: '125456789'.substring(0,4)});
-  chrome.browserAction.setBadgeText({text: i.toString() });
-   //chrome.browserAction.setBadgeBackGroundColor({color:[255, 0, 0, 255] });
- }
+  chrome.browserAction.setBadgeText({text: pointString.toString() });
+}//end UpdateBadge
