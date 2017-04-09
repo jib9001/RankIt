1a34fdcd8aafd40aff96a371d518349cb3760667
Brad <bruceba@mail.uc.edu>
2017-04-08 21:22:53 -0400

    Brad BadgeText Update v0.5
    Testing badge update attribute within extension

--- chromeExtensionTemplate/src/bg/background.js
+++ chromeExtensionTemplate/src/bg/background.js
@@ -11,3 +11,21 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+
+window.onload = function() {
+  UpdateBadge();
+}
+
+var test = parseint(4000000);
+var testsub = test.substring(0, 1);
+var string = "string";
+//var tabId = sender.tab
+
+function UpdateBadge(){
+  //chrome.browserAction.setBadgeText({text: '125456789'.substring(0,4)});
+  chrome.browserAction.setBadgeText({text: "testsub" });
+
+
+   //chrome.browserAction.setBadgeBackGroundColor({color:[255, 0, 0, 255] });
+ }
