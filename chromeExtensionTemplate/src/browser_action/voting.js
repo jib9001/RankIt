3667df5682322c9cf3c5e0ba4f2bc704af3896c6
Connor Tellep <jib11@live.com>
2017-04-09 11:58:19 -0400

    Implemented logic to prevent constant voting

--- chromeExtensionTemplate/src/browser_action/voting.js
+++ chromeExtensionTemplate/src/browser_action/voting.js
@@ -4,6 +4,9 @@ var databaseScore;
 var tempScore = 0;
 var boolVote = null;
 
+var upVoted = false;
+var downVoted = false;
+
 getScore();
 
 //score.innerHTML = tempScore;
@@ -16,17 +19,21 @@ downVoteImg.addEventListener("click", downVote);
 
 
 function upVote() {
+if(!upVoted && !downVoted)
+{
+  upVoted = true;
+  function dbUpVote();
+}
+else if(!upVoted && downVoted)
+{
+  downVoted = false;
+  function dbUpVote();
+}
 
-  if (boolVote == null){
-    boolVote = true;
-  }
-  if (boolVote == false){
-    boolVote = null
-  }
-    score.innerHTML = boolToInt(boolVote);
+}
 
-  //getScore();
-/*
+function dbUpVote()
+{
   chrome.tabs.getSelected(null, function (tab) {
   var url = new URL(tab.url);
   var domain = url.hostname;
@@ -37,54 +44,37 @@ function upVote() {
   };
   xhttp.send();
 })
-*/
 }
 
 function downVote() {
 
-    if (boolVote == null){
-      boolVote = false;
-    }
-    if (boolVote == true){
-      boolVote = null;
-    }
-    score.innerHTML = boolToInt(boolVote);
+  if(!upVoted && !downVoted)
+  {
+    downVoted = true;
+    function dbUpVote();
+  }
+  else if(upVoted && !downVoted)
+  {
+    upVoted = false;
+    function dbUpVote();
+  }
 
-
-    //getScore();
-/*
-    chrome.tabs.getSelected(null, function (tab) {
-    var url = new URL(tab.url);
-    var domain = url.hostname;
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET","http://phenom.servegame.com/RankIt/DownVote.php?domain=" + domain,true);
-    xhttp.onreadystatechange=function(){
-      score.innerHTML = xhttp.responseText;
-    };
-    xhttp.send();
-  })
-  */
 }
 
-chrome.tabs.getSelected(null, function (tab) {
-  var url = new URL(tab.url)
-  var domain = url.hostname
-  return domain;
-});
-
-function boolToInt(booleanVar) {
-  switch (booleanVar) {
-    case true:
-      tempScore = 1;
-      break;
-    case null:
-      tempScore = 0;
-      break;
-    case false:
-      tempScore = -1;
-      break;
-  }
-      return parseInt(databaseScore,10) + tempScore;
+function dbDownVote()
+{
+  chrome.tabs.getSelected(null, function (tab) {
+  var url = new URL(tab.url);
+  var domain = url.hostname;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET","http://phenom.servegame.com/RankIt/DownVote.php?domain=" + domain,true);
+  xhttp.onreadystatechange=function(){
+    score.innerHTML = xhttp.responseText;
+  };
+  xhttp.send();
+})
+}
+      //return parseInt(databaseScore,10) + tempScore;
 }
 
 function getScore(){
