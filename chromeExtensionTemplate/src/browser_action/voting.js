2ccb7b17895fd85d732ea867ec462ef7531e399b
BryanLikesToProgram <bryan.racic@angrysquirrels.com>
2017-04-09 14:15:06 -0400

    Implementing Home Button

--- chromeExtensionTemplate/src/browser_action/voting.js
+++ chromeExtensionTemplate/src/browser_action/voting.js
@@ -4,36 +4,37 @@ var databaseScore;
 var tempScore = 0;
 var boolVote = null;
 
-var upVoted = false;
-var downVoted = false;
-
 getScore();
 
 //score.innerHTML = tempScore;
 
 var upVoteImg= document.getElementById("upVoteImg");
 var downVoteImg = document.getElementById("downVoteImg");
+var houseImg = document.getElementById("HouseIcon");
+
 
 upVoteImg.addEventListener("click", upVote);
 downVoteImg.addEventListener("click", downVote);
+houseImg.addEventListener("click", housePress);
 
+function housePress(){
+  window.open('http://www.google,com/', '_blank')
+}
 
 function upVote() {
-if(!upVoted && !downVoted)
-{
-  upVoted = true;
-  dbUpVote();
-}
-else if(!upVoted && downVoted)
-{
-  downVoted = false;
-  dbUpVote();
-}
 
-}
+  if (boolVote == null){
+    boolVote = true;
+  }
+  if (boolVote == false){
+    boolVote = null
+  }
+    score.innerHTML = boolToInt(boolVote);
 
-function dbUpVote()
-{
+
+
+  //getScore();
+/*
   chrome.tabs.getSelected(null, function (tab) {
   var url = new URL(tab.url);
   var domain = url.hostname;
@@ -43,39 +44,61 @@ function dbUpVote()
     score.innerHTML = xhttp.responseText;
   };
   xhttp.send();
-});
+})
+*/
 }
 
 function downVote() {
 
-  if(!upVoted && !downVoted)
-  {
-    downVoted = true;
-    dbDownVote();
-  }
-  else if(upVoted && !downVoted)
-  {
-    upVoted = false;
-    dbDownVote();
-  }
+    if (boolVote == null){
+      boolVote = false;
+    }
+    if (boolVote == true){
+      boolVote = null;
+    }
+    score.innerHTML = boolToInt(boolVote);
 
+
+    //getScore();
+/*
+    chrome.tabs.getSelected(null, function (tab) {
+    var url = new URL(tab.url);
+    var domain = url.hostname;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","http://phenom.servegame.com/RankIt/DownVote.php?domain=" + domain,true);
+    xhttp.onreadystatechange=function(){
+      score.innerHTML = xhttp.responseText;
+    };
+    xhttp.send();
+  })
+  */
 }
 
-function dbDownVote()
-{
-  chrome.tabs.getSelected(null, function (tab) {
-  var url = new URL(tab.url);
-  var domain = url.hostname;
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("GET","http://phenom.servegame.com/RankIt/DownVote.php?domain=" + domain,true);
-  xhttp.onreadystatechange=function(){
-    score.innerHTML = xhttp.responseText;
-  };
-  xhttp.send();
-})
-}
-      //return parseInt(databaseScore,10) + tempScore;
+chrome.tabs.getSelected(null, function (tab) {
+  var url = new URL(tab.url)
+  var domain = url.hostname
+  return domain;
+});
 
+function boolToInt(booleanVar) {
+  switch (booleanVar) {
+    case true:
+      tempScore = 1;
+      document.getElementById("upVoteImg").src="upVoteGreen.png";
+      break;
+    case null:
+      tempScore = 0;
+      document.getElementById("upVoteImg").src="upvote.png";
+      document.getElementById("downVoteImg").src="downvote.png";
+
+      break;
+    case false:
+      tempScore = -1;
+      document.getElementById("downVoteImg").src="downVoteRed.png";
+      break;
+  }
+      return parseInt(databaseScore,10) + tempScore;
+}
 
 function getScore(){
   chrome.tabs.getSelected(null, function (tab) {
